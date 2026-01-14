2fdfd87102005a1bf5849c23f1442c0d44f31734
libo <libo@webull.com>
2026-01-14 16:39:19 +0800

    bugfix:admin后台新增用户，写入的密码是明文，导致无法登陆

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from .models import User, UserProfile
 
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name', 'department', 'position', 'is_active', 'date_joined']
     list_filter = ['is_active', 'department', 'position']
     search_fields = ['username', 'email', 'first_name', 'last_name']
